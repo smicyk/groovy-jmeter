a9b0bed65e75f952347f12bd58928be3262db5bc
smicyk <szymon.micyk@gmail.com>
2020-10-28 23:41:49 +0100

    [change] - update documentation with examples (#5)
    * [change] - update documentation with examples

--- examples/structure/script.groovy
+++ examples/structure/script.groovy
@@ -14,6 +14,7 @@ start {
         group(users: 1000, rampUp: 300) {
             cookies(name: 'cookies manager')
 
+            // insert login fragment
             insert 'fragments/login.groovy'
 
             http('GET /api/books') {
@@ -29,14 +30,16 @@ start {
             // simple form without parenthesis
             http 'GET /api/authors/${var_authorId}'
 
-            // if controller with expression and posting json data
+            // insert common fragment
             insert 'fragments/random_group.groovy'
 
+            // insert other common fragment
             insert 'fragments/loop.groovy'
         }
 
         summary(path: 'script.jtl', enabled: true)
 
+        // insert common configuration for backed listener
         insert 'fragments/backend.groovy'
     }
 }
