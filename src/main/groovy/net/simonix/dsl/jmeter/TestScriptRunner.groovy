0b9bd6db832e9470930bf54e36781577f042e60b
smicyk <szymon.micyk@gmail.com>
2021-03-06 23:17:58 +0100

    [fix] - take into account windows file separator when setting search path for jmeter functions (#24)

--- src/main/groovy/net/simonix/dsl/jmeter/TestScriptRunner.groovy
+++ src/main/groovy/net/simonix/dsl/jmeter/TestScriptRunner.groovy
@@ -33,7 +33,7 @@ import java.util.regex.Pattern
 @CompileDynamic
 final class TestScriptRunner {
 
-    final static Pattern JMETER_JARPATHS_MATCHER = ~/.*\/ApacheJMeter_[A-Za-z-.0-9]+\.jar$/
+    final static Pattern JMETER_JARPATHS_MATCHER = ~/.*(\/|\\)ApacheJMeter_[A-Za-z-.0-9]+\.jar$/
 
     final static String JMETER_PROPERITES_FILE = 'jmeter.properties'
     final static String JMETER_SAVESERVICE_FILE = 'saveservice.properties'
