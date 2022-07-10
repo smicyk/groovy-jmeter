fef267a4bbb95248b8e1f0c2930fa483b6fb540e
smicyk <szymon.micyk@gmail.com>
2022-07-10 22:33:45 +0200

    [fix] - debug properties not saved to jmx file (#97)
    * [fix] - debug properties not saved to jmx file
    
    - all values for debug sampler was not saved during jmx out operation
    
    * [fix] - debug properties not saved to jmx file
    
    - all values for debug sampler was not saved during jmx out operation

--- src/main/groovy/net/simonix/dsl/jmeter/factory/sampler/DebugFactory.groovy
+++ src/main/groovy/net/simonix/dsl/jmeter/factory/sampler/DebugFactory.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Szymon Micyk
+ * Copyright 2022 Szymon Micyk
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,5 +54,9 @@ final class DebugFactory extends TestElementNodeFactory {
         testElement.displayJMeterProperties = config.displayJMeterProperties
         testElement.displayJMeterVariables = config.displayJMeterVariables
         testElement.displaySystemProperties = config.displaySystemProperties
+
+        testElement.setProperty('displayJMeterProperties', config.displayJMeterProperties)
+        testElement.setProperty('displayJMeterVariables', config.displayJMeterVariables)
+        testElement.setProperty('displaySystemProperties', config.displaySystemProperties)
     }
 }
