f0ca7616687abc5204a7de4cb383bc1a4c87985c
smicyk <szymon.micyk@gmail.com>
2022-08-31 18:29:34 +0200

    [feature] - add java request sampler
    - new sampler to execute java_request
    - example how to create own java request (with groovy script)

--- src/test/groovy/net/simonix/dsl/jmeter/factory/sampler/DebugSamplerSpec.groovy
+++ src/test/groovy/net/simonix/dsl/jmeter/factory/sampler/DebugSamplerSpec.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.simonix.dsl.jmeter.factory.sampler;
+package net.simonix.dsl.jmeter.factory.sampler
 
 import net.simonix.dsl.jmeter.test.spec.TempFileSpec
 
